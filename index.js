d5976933880f4098ca396feb2645e51dc9cb5397
caleb <kalebntoyian2004@gmail.com>
2024-07-23 14:30:35 +0300

    labs/phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// index.js
 
-// Write your code here
+// Define variables for multiply test case
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
+
+// Define a variable for random test case
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+// Define variables for mod test case
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // 16 % 6 = 4
+
+// Define a variable for max test case
+let max = Math.max(10, 5, 20, 15); // 20 is the highest number in the set
